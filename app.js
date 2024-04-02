c22fe399060ec319c4a0e4e64477cb60fbc2a186
temomaglakelidze <temomaghlakelidze37b@geolab.edu.ge>
2024-04-02 19:29:15 +0400

    finished

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
 var firstName = "temo";
 var secondName = "maglakelidze";
-
 var fullName = firstName + secondName;
 
 var uppercaseFullName = fullName.toUpperCase();
-
 console.log(uppercaseFullName);
 
-let number = [];
-
-for (var i = 1; i <= 100; i++) {
-  number.push(i);
-}
-number.sort(function (a, b) {
-  return b - a;
-});
-
+let number = Math.floor(Math.random() * 100);
 console.log(number);
 
 let smallNumber = [1, 2, 3, 4, 5];
-
-let firstNumber = smallNumber.push;
 console.log(smallNumber);
+
+let lastNumber = smallNumber[4];
+console.log(lastNumber);
+
+let obj = {
+  id: 1,
+  name: "hello",
+};
